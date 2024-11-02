1ff5a6eb22d51f95bc3b8c47a2191802797a84d5
Chidera Ulu <chideraulu@gmail.com>
2024-11-02 18:26:12 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Welcome from './Components/Welcome/Welcome'
+
+const App = () => {
+  return (
+    <div>
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Welcome/Welcome.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Logo from '../../assets/N Logo.png'
+
+const Welcome = () => {
+  return (
+    <div>
+
+      <div>
+        <div>
+            <img src={Logo} alt="" />
+            <h2>ThireNow</h2>
+        </div>
+
+        <div>
+            <h2></h2>
+            <p></p>
+        </div>
+
+        <div>
+            <input type="email" name="" id="" placeholder='support@gmail.com' />
+            <button>Join Waitlist</button>
+        </div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default Welcome
