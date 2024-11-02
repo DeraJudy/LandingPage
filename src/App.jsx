817564a44ac00a309327e645a048dc42d301ad13
Chidera Ulu <chideraulu@gmail.com>
2024-11-02 20:05:50 +0100

    background- text & image

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./App.css"
 import Welcome from './Components/Welcome/Welcome'
 
 const App = () => {
--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -1,29 +1,37 @@
 import React from 'react'
+import './Welcome.css'
+
 import Logo from '../../assets/N Logo.png'
 
 const Welcome = () => {
   return (
-    <div>
+    <div className='container'>
 
-      <div>
-        <div>
-            <img src={Logo} alt="" />
-            <h2>ThireNow</h2>
+        <div className='image-container'>
+            <img src={Logo} alt=""  className='image'/>
+            <h2 className='image-text'>ThireNow</h2>
         </div>
 
-        <div>
-            <h2></h2>
-            <p></p>
+        <div className='text-container'>
+            <h2 className='big-text'>
+                Unlock Your Tech Career Potential
+                {/* <hr className='line'/> */}
+                <br/> <span>Join the Future of Remote Work with ThireNow</span> 
+            </h2>
+            <p className='small-text'>
+                Get AI-powered job matches, access free resources, 
+                and be part of a thriving tech community.<br/>
+                Be the First to Know - Join Our Exclusive Launch List!
+            </p>
         </div>
 
-        <div>
+        <div className='button-container'>
             <input type="email" name="" id="" placeholder='support@gmail.com' />
             <button>Join Waitlist</button>
         </div>
-        
-      </div>
 
     </div>
+
   )
 }
 
